3c2740ef55c73cedd951d8185c4870b440eb5f90
hunmengyuanxue <103264657+hunmengyuanxue@users.noreply.github.com>
2022-07-30 21:01:37 +0800

    Create basketball.py

---
+++ play/basketball.py
@@ -0,0 +1,35 @@
+import pygame
+
+
+class Snake(pygame.sprite.Sprite):
+    def __init__(self, filename, location):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(filename)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = location
+
+a=0
+pygame.init()
+screen = pygame.display.set_mode(size=(800, 600), flags=pygame.RESIZABLE)
+screen.fill((60, 240, 250))
+pygame.display.set_caption('魂梦怨雪test')
+pygame.display.set_icon(pygame.image.load("sa.jpg"))
+filename=("ball.png")
+location = (100, 150)
+b = Snake(filename, location)
+while True:
+    if (a==1):
+        break
+    for event in pygame.event.get():
+        if (event.type == pygame.QUIT):
+            a=1
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            location = (x, y)
+            b = Snake(filename, location)
+            screen.blit(b.image, b.rect)
+        if (event.type == pygame.KEYDOWN):
+            if (event.key == pygame.K_ESCAPE):
+                a=1
+    pygame.display.flip()
+pygame.quit()
